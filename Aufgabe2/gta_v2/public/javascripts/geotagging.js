05730ff6977f130e9439324eed1f054bd0505a42
Matthias Harzer <matthias.harzer03@gmail.com>
2024-04-25 11:44:44 +0200

    A2: chore: fix merge errors and replace Google Maps Static with OpenLayers

--- Aufgabe2/gta_v2/public/javascripts/geotagging.js
+++ Aufgabe2/gta_v2/public/javascripts/geotagging.js
@@ -1,4 +1,3 @@
-import { GOOGLE_MAPS_STATIS_API_KEY } from "./secrets.js";
 // File origin: VS1LAB A2
 
 /* eslint-disable no-unused-vars */
@@ -37,6 +36,16 @@ class LocationHelper {
   }
 
   /**
+   * Create LocationHelper instance if coordinates are known.
+   * @param {string} latitude
+   * @param {string} longitude
+   */
+  constructor(latitude, longitude) {
+    this.#latitude = parseFloat(latitude).toFixed(5);
+    this.#longitude = parseFloat(longitude).toFixed(5);
+  }
+
+  /**
    * The 'findLocation' method requests the current location details through the geolocation API.
    * It is a static method that should be used to obtain an instance of LocationHelper.
    * Throws an exception if the geolocation API is not available.
@@ -48,85 +57,69 @@ class LocationHelper {
     if (!geoLocationApi) {
       throw new Error("The GeoLocation API is unavailable.");
     }
-   /**
-    * Create LocationHelper instance if coordinates are known.
-    * @param {string} latitude 
-    * @param {string} longitude 
-    */
-   constructor(latitude, longitude) {
-       this.#latitude = (parseFloat(latitude)).toFixed(5);
-       this.#longitude = (parseFloat(longitude)).toFixed(5);
-   }
 
-    /**
-     * The 'findLocation' method requests the current location details through the geolocation API.
-     * It is a static method that should be used to obtain an instance of LocationHelper.
-     * Throws an exception if the geolocation API is not available.
-     * @param {*} callback a function that will be called with a LocationHelper instance as parameter, that has the current location details
-     */
-    static findLocation(callback) {
-        const geoLocationApi = navigator.geolocation;
-
-        if (!geoLocationApi) {
-            throw new Error("The GeoLocation API is unavailable.");
-        }
-
-        // Call to the HTML5 geolocation API.
-        // Takes a first callback function as argument that is called in case of success.
-        // Second callback is optional for handling errors.
-        // These callbacks are given as arrow function expressions.
-        geoLocationApi.getCurrentPosition((location) => {
-            // Create and initialize LocationHelper object.
-            let helper = new LocationHelper(location.coords.latitude, location.coords.longitude);
-            // Pass the locationHelper object to the callback.
-            callback(helper);
-        }, (error) => {
-           alert(error.message)
-        });
-    }
+    // Call to the HTML5 geolocation API.
+    // Takes a first callback function as argument that is called in case of success.
+    // Second callback is optional for handling errors.
+    // These callbacks are given as arrow function expressions.
+    geoLocationApi.getCurrentPosition(
+      (location) => {
+        // Create and initialize LocationHelper object.
+        let helper = new LocationHelper(
+          location.coords.latitude,
+          location.coords.longitude
+        );
+        // Pass the locationHelper object to the callback.
+        callback(helper);
+      },
+      (error) => {
+        alert(error.message);
+      }
+    );
+  }
 }
 
 /**
  * A class to help using the Leaflet map service.
  */
 class MapManager {
-    #map
-    #markers
+  #map;
+  #markers;
 
-    /**
-    * Initialize a Leaflet map
-    * @param {number} latitude The map center latitude
-    * @param {number} longitude The map center longitude
-    * @param {number} zoom The map zoom, defaults to 18
-    */
-    initMap(latitude, longitude, zoom = 18) {
-        // set up dynamic Leaflet map
-        this.#map = L.map('map').setView([latitude, longitude], zoom);
-        var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        L.tileLayer(
-            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; ' + mapLink + ' Contributors'}).addTo(this.#map);
-        this.#markers = L.layerGroup().addTo(this.#map);
-    }
+  /**
+   * Initialize a Leaflet map
+   * @param {number} latitude The map center latitude
+   * @param {number} longitude The map center longitude
+   * @param {number} zoom The map zoom, defaults to 18
+   */
+  initMap(latitude, longitude, zoom = 18) {
+    // set up dynamic Leaflet map
+    this.#map = L.map("map").setView([latitude, longitude], zoom);
+    var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+    L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; " + mapLink + " Contributors",
+    }).addTo(this.#map);
+    this.#markers = L.layerGroup().addTo(this.#map);
+  }
 
-    /**
-    * Update the Markers of a Leaflet map
-    * @param {number} latitude The map center latitude
-    * @param {number} longitude The map center longitude
-    * @param {{latitude, longitude, name}[]} tags The map tags, defaults to just the current location
-    */
-    updateMarkers(latitude, longitude, tags = []) {
-        // delete all markers
-        this.#markers.clearLayers();
-        L.marker([latitude, longitude])
-            .bindPopup("Your Location")
-            .addTo(this.#markers);
-        for (const tag of tags) {
-            L.marker([tag.location.latitude,tag.location.longitude])
-                .bindPopup(tag.name)
-                .addTo(this.#markers);  
-        }
+  /**
+   * Update the Markers of a Leaflet map
+   * @param {number} latitude The map center latitude
+   * @param {number} longitude The map center longitude
+   * @param {{latitude, longitude, name}[]} tags The map tags, defaults to just the current location
+   */
+  updateMarkers(latitude, longitude, tags = []) {
+    // delete all markers
+    this.#markers.clearLayers();
+    L.marker([latitude, longitude])
+      .bindPopup("Your Location")
+      .addTo(this.#markers);
+    for (const tag of tags) {
+      L.marker([tag.location.latitude, tag.location.longitude])
+        .bindPopup(tag.name)
+        .addTo(this.#markers);
     }
+  }
 }
 
 /**
@@ -142,21 +135,20 @@ const latitudeElement = document.querySelector("#tag-lat");
  * @type {HTMLInputElement}
  */
 const longitudeElement = document.querySelector("#tag-lon");
-/**
- * @type {HTMLImageElement}
- */
-const mapPreviewElement = document.querySelector("#mapView");
+const mapPreviewElement = document.querySelector("#mapPreview");
+const mapPreviewLabelElement = document.querySelector("#mapPreviewLabel");
 
 function updateLocation() {
   LocationHelper.findLocation((helper) => {
     latitudeElement.value = helper.latitude;
     longitudeElement.value = helper.longitude;
 
-    const mapManager = new MapManager(GOOGLE_MAPS_STATIS_API_KEY);
-    mapPreviewElement.src = mapManager.getMapUrl(
-      helper.latitude,
-      helper.longitude
-    );
+    mapPreviewElement.remove();
+    mapPreviewLabelElement.remove();
+
+    const mapManager = new MapManager();
+    mapManager.initMap(helper.latitude, helper.longitude);
+    mapManager.updateMarkers(helper.latitude, helper.longitude);
   });
 }
 
