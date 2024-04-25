c7086f29fc83f00db94da41503eed248803f7a50
Matthias Harzer <matthias.harzer03@gmail.com>
2024-04-25 11:23:40 +0200

    A3: chore: setup

--- Aufgabe3/gta_v3/models/geotag-store.js
+++ Aufgabe3/gta_v3/models/geotag-store.js
@@ -7,26 +7,24 @@
 
 /**
  * A class for in-memory-storage of geotags
- * 
+ *
  * Use an array to store a multiset of geotags.
  * - The array must not be accessible from outside the store.
- * 
+ *
  * Provide a method 'addGeoTag' to add a geotag to the store.
- * 
+ *
  * Provide a method 'removeGeoTag' to delete geo-tags from the store by name.
- * 
+ *
  * Provide a method 'getNearbyGeoTags' that returns all geotags in the proximity of a location.
  * - The location is given as a parameter.
  * - The proximity is computed by means of a radius around the location.
- * 
+ *
  * Provide a method 'searchNearbyGeoTags' that returns all geotags in the proximity of a location that match a keyword.
  * - The proximity constrained is the same as for 'getNearbyGeoTags'.
- * - Keyword matching should include partial matches from name or hashtag fields. 
+ * - Keyword matching should include partial matches from name or hashtag fields.
  */
-class InMemoryGeoTagStore{
-
-    // TODO: ... your code here ...
-
+class InMemoryGeoTagStore {
+  #tags = [];
 }
 
-module.exports = InMemoryGeoTagStore
+module.exports = InMemoryGeoTagStore;
